1b88cdff7971d135bb7a82b7406056a381d042c3
abdyrakhman <medzholdoshbaev@gmail.com>
2024-07-15 20:16:13 +0600

    call class in main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
+import utils.Server;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            new Server("localhost", 9879).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
